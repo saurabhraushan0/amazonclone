42daf2c4c1f8773ec566a064cf71544cca465457
saurabh <saurabh77420@gmail.com>
2023-10-22 18:06:25 +0530

    commit1

--- data/product-html.js
+++ data/product-html.js
@@ -8,7 +8,7 @@ products.forEach((product)=>{
   <div class="name-container">
     ${product.name}
   </div>
-  <div class="ratings"><img src="rating-${product.rating.star*10}.png" alt=""> <span class="ratings-number">${product.rating.count}</span>
+  <div class="ratings"><img src="images/ratings/rating-${product.rating.stars*10}.png" alt="cat"> <span class="ratings-number">${product.rating.count}</span>
   </div>
   <div class="product-price">$${product.priceCents/100}</div>
   <div class="product-quantity">
@@ -26,11 +26,11 @@ products.forEach((product)=>{
     </select>
   </div>
   <div class="spacer"></div>
-  <div class="added">
+  <div class="added js-add-${product.id}">
     <img src="checkmark.png" alt="">
     <span>Added</span>
   </div>
-  <button class="add" data-product-id="${product.id}">Add to cart </button>
+  <button class="add " data-product-id="${product.id}">Add to cart </button>
 </div>`;
 })
 document.querySelector('.main-grid').innerHTML=productHTML;
@@ -70,11 +70,16 @@ document.querySelectorAll('.add').forEach((button)=>{
       })
       
       let priceCent=product_item.priceCents
-      console.log(priceCent);
       cart.push({id:id,quantity:1,image:`${product_item.image}`,name:`${product_item.name}`,price:priceCent,shippingCents:0});
     }
     saveToStorage();
     cartquantity();
+    const button1=document.querySelector(`.js-add-${id}`);
+    button1.classList.add('product-added');
+    console.log(button1);
+    setTimeout(()=>{
+      button1.classList.remove("product-added");
+    },2000);
   })
 })
 
